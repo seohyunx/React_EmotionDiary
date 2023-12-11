2d792f47639c1da587d6f5052f5783ec0af5fbc3
seohyunx <sh0501sh11@gmail.com>
2023-12-11 23:36:55 +0900

    feat: Diary 컴포넌트 라우팅 - Path Variable(useParams) 사용

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
         {/* <a href={"/new"}>NEW로 이동</a> */}
         <RouteTest />
