95fa53e2dee1bc2da7f9fb5837251194349c2167
seohyunx <sh0501sh11@gmail.com>
2023-12-12 17:16:08 +0900

    feat: useSearchParams, useNavigate 사용 - 페이지 라우팅 마무리

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
+          {/* Path Variable: URL의 일부를 변수로 사용하는 방식 */}
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
         {/* <a href={"/new"}>NEW로 이동</a> */}
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  //Query String 사용
+  //useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
+  //useNavigate()
+  // 페이지를 이동 시킬 수 있는 함수를 하나 반환
+  // navigate 기능은 링크 태그를 클릭 안 했을 때도 의도적으로 페이지를 바꿔버릴 수 있음
+  // ex)로그인이 안된 사용자가 로그인을 시도할 때, 로그인값 검사를 해서
+  // 로그인을 안 했다면 로그인페이지로 강제로 보내버릴 때 사용
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이 곳은 일기수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "hyunrok" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1); // -1 : 한번 뒤로가기 의미
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
